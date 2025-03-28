3e1dc873e9044e7d9ec04f7b8484cb36ce665388
sinasll <sinasalah6@gmail.com>
2025-03-29 02:25:37 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAzXSCn_QL2XeyRZD71By443sl4wOtXf2Y",
+    authDomain: "pipcore-8844f.firebaseapp.com",
+    projectId: "pipcore-8844f",
+    storageBucket: "pipcore-8844f.firebasestorage.app",
+    messagingSenderId: "921115337984",
+    appId: "1:921115337984:web:17161651342ad78017bfe5",
+    measurementId: "G-RKL37B0B7N"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
